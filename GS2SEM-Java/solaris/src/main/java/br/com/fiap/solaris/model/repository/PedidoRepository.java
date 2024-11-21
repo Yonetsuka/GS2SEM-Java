44c7d7fc46b982f6b96c4d3e5de00b3a465c73b4
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-11-21 11:09:26 -0300

    Update PedidoRepository.java

--- GS2SEM-Java/solaris/src/main/java/br/com/fiap/solaris/model/repository/PedidoRepository.java
+++ GS2SEM-Java/solaris/src/main/java/br/com/fiap/solaris/model/repository/PedidoRepository.java
@@ -1,3 +1,8 @@
+/*Nome:Cássio Eid Kobayashi Yonetsuka
+Rm:99678
+
+Nome: Allan Von Ivanov
+Rm:98705 */
 package br.com.fiap.solaris.model.repository;
 
 import br.com.fiap.solaris.model.entity.Item;
